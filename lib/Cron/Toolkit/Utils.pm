38e7d053e09963053dc0272458ed3f060028c779
Nate Graham <ngraham@cpan.org>
2025-11-18 11:56:41 -0600

    cleanup and removal of old code

--- lib/Cron/Toolkit/Utils.pm
+++ lib/Cron/Toolkit/Utils.pm
@@ -3,14 +3,12 @@ use strict;
 use warnings;
 use Exporter qw(import);
 our @EXPORT_OK = qw(
-  format_time num_to_ordinal field_unit join_parts fill_template is_midnight time_suffix quartz_dow
-  quartz_dow_normalize unix_dow_normalize %aliases %field_names %allowed_chars %allowed_types
-  ordinal_list step_ordinal complex_join normalize generate_list_desc %limits %DOW_MAP_UNIX @FIELDS %LIMITS
-  %MONTH_MAP %DOW_MAP_QUARTZ %MONTH_NAMES %DAY_NAMES %ALLOWED_CHARS %ALIASES %nth_names %unit_labels %ordinal_suffix %joiners %templates
+  format_time num_to_ordinal %DOW_MAP_UNIX %DOW_MAP_QUARTZ %MONTH_MAP 
+  %LIMITS %DOW_MAP_QUARTZ %MONTH_NAMES %DAY_NAMES %ALLOWED_CHARS %ALIASES
 );
 
 our %EXPORT_TAGS = ( all => [@EXPORT_OK] );
-our %MONTH_MAP = (
+our %MONTH_MAP   = (
    JAN       => 1,
    JANUARY   => 1,
    FEB       => 2,
@@ -85,32 +83,18 @@ our %MONTH_NAMES = (
    12 => 'December'
 );
 
-our %DAY_NAMES   = ( 0 => 'Sunday', 1 => 'Monday', 2 => 'Tuesday', 3 => 'Wednesday', 4 => 'Thursday', 5 => 'Friday', 6 => 'Saturday' );
-our %nth_names   = ( 1 => 'first',  2 => 'second', 3 => 'third',   4 => 'fourth',    5 => 'fifth' );
-our %field_names = ( 0 => 'second', 1 => 'minute', 2 => 'hour',    3 => 'dom',       4 => 'month', 5 => 'dow', 6 => 'year' );
-
-our %unit_labels = (
-   second => [ 'second',          'seconds' ],
-   minute => [ 'minute',          'minutes' ],
-   hour   => [ 'hour',            'hours' ],
-   dom    => [ 'day',             'days' ],
-   month  => [ 'month',           'months' ],
-   dow    => [ 'day of the week', 'days of the week' ],
-   year   => [ 'year',            'years' ]
-);
+our %DAY_NAMES = ( 0 => 'Sunday', 1 => 'Monday', 2 => 'Tuesday', 3 => 'Wednesday', 4 => 'Thursday', 5 => 'Friday', 6 => 'Saturday' );
 
 # Exported constants — UPPERCASE
 our %LIMITS = (
-    second => [0, 59],
-    minute => [0, 59],
-    hour   => [0, 23],
-    dom    => [1, 31],
-    month  => [1, 12],
-    dow    => [0, 7],
-    year   => [1970, 2099],
+   second => [ 0,    59 ],
+   minute => [ 0,    59 ],
+   hour   => [ 0,    23 ],
+   dom    => [ 1,    31 ],
+   month  => [ 1,    12 ],
+   dow    => [ 0,    7 ],
+   year   => [ 1970, 2099 ],
 );
-
-our @FIELDS = qw(second minute hour dom month dow year);
 
 our %ALLOWED_CHARS = (
    second => qr/^[0-9,\*\/\-]+$/,
@@ -133,34 +117,26 @@ our %ALIASES = (
 );
 
 sub num_to_ordinal {
-    my $n = shift;
-    return $n . (
-        $n % 10 == 1 && $n != 11 ? 'st' :
-        $n % 10 == 2 && $n != 12 ? 'nd' :
-        $n % 10 == 3 && $n != 13 ? 'rd' : 'th'
-    );
+   my $n = shift;
+   return $n
+     . (
+        $n % 10 == 1 && $n != 11 ? 'st'
+      : $n % 10 == 2 && $n != 12 ? 'nd'
+      : $n % 10 == 3 && $n != 13 ? 'rd'
+      :                            'th'
+     );
 }
 
 sub format_time {
-    my ($sec, $min, $hour) = @_;
-    return "midnight" if $hour == 0 && $min == 0 && $sec == 0;
-    my $ampm = $hour >= 12 ? 'PM' : 'AM';
-    $hour = $hour % 12 || 12;
-    return   $min && $sec ? sprintf("%d:%02d:%02d %s", $hour, $min, $sec, $ampm) 
-           : $min ? sprintf("%d:%02d %s", $hour, $min, $ampm)
-           : "$hour $ampm";
+   my ( $sec, $min, $hour ) = @_;
+   return "midnight" if $hour == 0 && $min == 0 && $sec == 0;
+   my $ampm = $hour >= 12 ? 'PM' : 'AM';
+   $hour = $hour % 12 || 12;
+   return
+       $min && $sec ? sprintf( "%d:%02d:%02d %s", $hour, $min, $sec, $ampm )
+     : $min         ? sprintf( "%d:%02d %s", $hour, $min, $ampm )
+     :                "$hour $ampm";
 
 }
-
-sub is_midnight { my ( $h, $m, $s ) = @_; return $h == 0 && $m == 0 && $s == 0; }
-sub time_suffix { my $h = shift; return $h == 0 ? 'midnight' : $h == 12 ? 'noon' : ''; }
-sub field_unit  { my ( $f, $c ) = @_; $c //= 1; my ( $s, $p ) = @{ $unit_labels{$f} }; return $c == 1 ? $s : $p; }
-sub quartz_dow  { my ($iso_dow) = @_; return $iso_dow == 7 ? 1 : $iso_dow + 1; }
-
-sub ordinal_list {
-   join( ', ', map { num_to_ordinal($_) } @_ );
-}
-sub step_ordinal { my $n = shift; return $n . ( $n == 1 ? 'st' : $n == 2 ? 'nd' : $n == 3 ? 'rd' : 'th' ); }
-sub complex_join { join( ', ', @_ ) . ' at {time}'; }
 
 1;
--- t/verify_next.t
+++ t/verify_next.t
@@ -14,6 +14,12 @@ diag "Base time: " . $base->strftime('%Y-%m-%d %H:%M:%S UTC') . " (epoch $base_e
 
 my @tests = (
     {
+        expr => '* * * 8 * 2#2 *',
+        desc => 'dow dom',
+        next_tm => $base->plus_seconds(1),
+        prev_tm => $base->minus_seconds(1),
+    },
+    {
         expr => '* * 3,16-22 * * ? *',
         desc => 'every second',
         next_tm => $base->plus_seconds(1),
